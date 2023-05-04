a2bf8e928b259152517a99534da4fac99e085845
Vladarius <logvladarius@gmail.com>
2023-05-04 14:36:31 +0300

    jfdhg

--- main.py
+++ main.py
@@ -28,3 +28,37 @@ async def generate_jsons() -> List[Dict[str, str]]:
         image = choice(images)
         jsons.append({"title": title, "description": description, "image": image})
     return jsons
+
+APP_ID=531078899236306
+REDIRECT_URI='https://wladarius.github.io/vlada/shtory.html'
+INSTAGRAMM_URL=f'https://api.instagram.com/oauth/authorize?client_id={APP_ID}&redirect_uri={REDIRECT_URI}&scope=user_profile,user_media&response_type=code'
+
+#https://api.instagram.com/oauth/authorize?client_id=531078899236306&redirect_uri=https://wladarius.github.io/vlada/shtory.html&scope=user_profile,user_media&response_type=code
+
+
+
+# https://wladarius.github.io/vlada/shtory.html?code=AQDb106VyWpCisYynNvW155ZXr_Th0SLXmH6xVybsl23f2kRjZ87epmqIGT3EU-GeRQfT87QS7GGMoFiuupuXCoVh8lYj7LXrkGBFPxL10rlXCeFm2hwa0tyNLemkj8S238gBwRbctOhqgK3p2QBFQcGX4UsUZTslRz6kr_R42WWw5G0LwGaJc2DEeb7a8HmDp3TF6GGE4pSwFMjmSqNq1ImJ3CBVpxLo9LZCVP_wetrjA#_
+
+
+# curl -X POST \
+#   https://api.instagram.com/oauth/access_token \
+#   -F client_id=531078899236306 \
+#   -F client_secret=67e017b33763acf786084b185e3bade3 \
+#   -F grant_type=authorization_code \
+#   -F redirect_uri=https://wladarius.github.io/vlada/shtory.html \
+#   -F code=AQDb106VyWpCisYynNvW155ZXr_Th0SLXmH6xVybsl23f2kRjZ87epmqIGT3EU-GeRQfT87QS7GGMoFiuupuXCoVh8lYj7LXrkGBFPxL10rlXCeFm2hwa0tyNLemkj8S238gBwRbctOhqgK3p2QBFQcGX4UsUZTslRz6kr_R42WWw5G0LwGaJc2DEeb7a8HmDp3TF6GGE4pSwFMjmSqNq1ImJ3CBVpxLo9LZCVP_wetrjA#_
+
+# {"access_token": "IGQVJVNEczRUhJVzNLaUNIQ1E1R0tFWXRrVWNzQzgzZAF9rbHZADTWdfUDkzeFk4Q050UWN3Qm5hWXJaTFlDWUdqVGlzSVNHcVFlVlpfRGdlTFlWU1YyRWxMMlFrNkxQNjkzVlhodHlsU0NKajhrbkh2RFRQZA2ZA3aDlWRUVn", "user_id": 17841445544215336}%
+
+# curl -X GET \
+#   'https://graph.instagram.com/17841445544215336?fields=id,username&access_token=IGQVJVNEczRUhJVzNLaUNIQ1E1R0tFWXRrVWNzQzgzZAF9rbHZADTWdfUDkzeFk4Q050UWN3Qm5hWXJaTFlDWUdqVGlzSVNHcVFlVlpfRGdlTFlWU1YyRWxMMlFrNkxQNjkzVlhodHlsU0NKajhrbkh2RFRQZA2ZA3aDlWRUVn'
+
+
+# curl -X GET \
+#   'https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url&access_token=IGQVJVNEczRUhJVzNLaUNIQ1E1R0tFWXRrVWNzQzgzZAF9rbHZADTWdfUDkzeFk4Q050UWN3Qm5hWXJaTFlDWUdqVGlzSVNHcVFlVlpfRGdlTFlWU1YyRWxMMlFrNkxQNjkzVlhodHlsU0NKajhrbkh2RFRQZA2ZA3aDlWRUVn'
+
+#   caption
+# id
+# media_type
+# The Media's type. Can be IMAGE, VIDEO, or CAROUSEL_ALBUM.
+# media_url
